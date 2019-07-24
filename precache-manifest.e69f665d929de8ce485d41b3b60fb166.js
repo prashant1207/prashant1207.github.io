57255e4e2b580f20ce09d6769ee9233c9705be2c
Prashant Tiwari <prashant1207@gmail.com>
2019-07-24 14:46:09 +0200

    Initial Commit

---
+++ precache-manifest.e69f665d929de8ce485d41b3b60fb166.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e57e23d8ff250a5cce61a069847624d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b187cd490f221a3cd9e",
+    "url": "/static/css/2.676dc204.chunk.css"
+  },
+  {
+    "revision": "0ef560650e5d16d83f01",
+    "url": "/static/css/main.12d3fe2c.chunk.css"
+  },
+  {
+    "revision": "6b187cd490f221a3cd9e",
+    "url": "/static/js/2.6c4baca5.chunk.js"
+  },
+  {
+    "revision": "0ef560650e5d16d83f01",
+    "url": "/static/js/main.a42683cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
